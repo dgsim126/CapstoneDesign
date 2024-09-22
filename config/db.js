1fddb603e78511bf939ecb5298f63578f56293e8
dgsim <dgsim126@gmail.com>
2024-09-23 00:04:35 +0900

    now

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,  // 포트 설정 추가
     dialect: 'mariadb', // MariaDB 사용
     logging: false, // 쿼리 로깅 비활성화
     //timezone: '+09:00' // 한국 시간대 설정
